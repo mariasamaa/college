4841b05bc605a29f5a17333d1d9d8f5dd86348bf
Maria Fernanda Caetano <nanda2001caetano@gmail.com>
2024-04-16 10:16:11 -0300

    Updating exercises

---
+++ WebDevelopment/Tutorials/BSI5-SIC0223-TUTORIAL-02-MARIA/main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',  //valor atualizado
+            description: 'The best pair of boots you will ever own'
+        }
+    }
+})
